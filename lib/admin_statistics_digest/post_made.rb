a4049a8772c210c6228b5335d71734910a303f5c
scossar <scossar3@gmail.com>
2017-11-06 13:00:49 -0800

    Use active_range filter

--- lib/admin_statistics_digest/post_made.rb
+++ lib/admin_statistics_digest/post_made.rb
@@ -1,15 +1,16 @@
 require_relative '../admin_statistics_digest/base_report'
 
 class AdminStatisticsDigest::PostMade < AdminStatisticsDigest::BaseReport
-  # Todo: rename this
+  # Todo: maybe rename (alias) this?
   provide_filter :active_range
 
   def initialize
     super
   end
 
+  # Todo: maybe this only needs to return the count?
   def to_sql
-    posts = Post.all
+    posts = Post.where(created_at: filters.active_range.first..filters.active_range.last)
     posts.to_sql
   end
 
