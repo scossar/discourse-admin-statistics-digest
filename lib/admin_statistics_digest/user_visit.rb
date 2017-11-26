9083aff6acae5c286f4393f9d2a6c096e9252655
scossar <scossar3@gmail.com>
2017-11-26 13:04:08 -0800

    Remove days_in_period field

--- lib/admin_statistics_digest/user_visit.rb
+++ lib/admin_statistics_digest/user_visit.rb
@@ -4,15 +4,12 @@ class AdminStatisticsDigest::UserVisit < AdminStatisticsDigest::BaseReport
 
   provide_filter :months_ago
 
-  # All users, or number of users who have visited within a time period.
   def to_sql
     <<~SQL
 SELECT
-count(1) as "user_visits",
-EXTRACT(DAY FROM DATE '#{filters.months_ago[:period_end]}') AS "days_in_period"
+count(1) as "user_visits"
 FROM "user_visits" "uv"
 WHERE ("uv"."visited_at", "uv"."visited_at") OVERLAPS('#{filters.months_ago[:period_start]}', '#{filters.months_ago[:period_end]}')
     SQL
   end
-
 end
