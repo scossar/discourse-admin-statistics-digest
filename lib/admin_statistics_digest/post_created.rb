560feae5207845387f31594f9d91cd55e5f3b92c
scossar <scossar3@gmail.com>
2017-11-26 10:24:54 -0800

    Check the topic archetype before counting the post

--- lib/admin_statistics_digest/post_created.rb
+++ lib/admin_statistics_digest/post_created.rb
@@ -9,9 +9,11 @@ class AdminStatisticsDigest::PostCreated < AdminStatisticsDigest::BaseReport
 SELECT
 count(1) AS "posts_created"
 FROM "posts" "p"
+JOIN "topics" "t"
+ON "t"."id" = "p"."topic_id"
 WHERE ("p"."created_at", "p"."created_at") OVERLAPS('#{filters.months_ago[:period_start]}', '#{filters.months_ago[:period_end]}')
+AND "t"."archetype" = 'regular'
 AND "p"."user_id" > 0
     SQL
   end
-
 end
