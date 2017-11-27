34d9a3445e2c01839607ac0fad55b3ace5c4ad34
scossar <scossar3@gmail.com>
2017-11-27 14:03:00 -0800

    Run the job daily; return unless it's the first day of the month

--- app/jobs/admin_statistics_digest.rb
+++ app/jobs/admin_statistics_digest.rb
@@ -1,14 +1,13 @@
-# require_relative '../mailers/report_mailer'
-
 module ::Jobs
   class AdminStatisticsDigest < ::Jobs::Scheduled
-    every 1.minutes
+    every 1.day
     sidekiq_options 'retry' => true, 'queue' => 'critical'
 
     def execute(opts = nil)
+
       return unless DateTime.now.day == 1
 
-      ::AdminStatisticsDigest::ReportMailer.digest(30.days.ago.to_date, Date.today).deliver_now
+      ::AdminStatisticsDigest::ReportMailer.digest(1).deliver_now
     end
   end
 end
