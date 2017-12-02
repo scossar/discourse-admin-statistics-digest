e828617ca53ac6781e2569afb7819c2766a090b0
scossar <scossar3@gmail.com>
2017-12-01 21:48:00 -0800

    Return average visits from daily_active_user query

--- lib/admin_statistics_digest/daily_active_user.rb
+++ lib/admin_statistics_digest/daily_active_user.rb
@@ -6,32 +6,32 @@ class AdminStatisticsDigest::DailyActiveUser < AdminStatisticsDigest::BaseReport
 
   def to_sql
     <<~SQL
-WITH "periods" AS (
+WITH periods AS (
 SELECT
-"months_ago",
-date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * "months_ago" AS "period_start",
-date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * "months_ago" + INTERVAL '1 month' - INTERVAL '1 second' AS "period_end"
-FROM unnest(ARRAY #{filters.months_ago}) as "months_ago"
+months_ago,
+date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago AS period_start,
+date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago + INTERVAL '1 month' - INTERVAL '1 second' AS period_end,
+COALESCE(EXTRACT(DAY FROM (date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago + INTERVAL '1 month' - INTERVAL '1 second')), 1) AS days_in_period
+FROM unnest(ARRAY #{filters.months_ago}) AS months_ago
 ),
-"daily_visits" AS(    
+daily_visits AS(    
 SELECT
-count(1) AS "visits",
-"p"."months_ago",
-"p"."period_end"
-FROM "user_visits" "uv"
-JOIN "periods" "p"
-ON "uv"."visited_at" >= "p"."period_start"
-AND "uv"."visited_at" <= "p"."period_end"
-GROUP BY "p"."months_ago", "p"."period_end"
-ORDER BY "p"."months_ago"
+count(1) AS visits,
+p.months_ago,
+p.days_in_period as days_in_period
+FROM user_visits uv
+JOIN periods p
+ON uv.visited_at >= p.period_start
+AND uv.visited_at <= p.period_end
+GROUP BY p.months_ago, p.days_in_period
+ORDER BY p.months_ago
 )
 
 SELECT
-"dv"."months_ago",
-sum("dv"."visits") AS "daily_visits",
-"dv"."period_end"
-FROM "daily_visits" "dv"
-GROUP BY "dv"."months_ago", "dv"."period_end"
+dv.months_ago,
+sum(dv.visits) / dv.days_in_period AS average_visits
+FROM daily_visits dv
+GROUP BY dv.months_ago, dv.days_in_period
 SQL
     end
 end
