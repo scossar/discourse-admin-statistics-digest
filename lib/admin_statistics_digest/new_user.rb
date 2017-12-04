7038cf879b5105b0cb4c430a8accb36a1f5a4135
scossar <scossar3@gmail.com>
2017-12-03 17:29:55 -0800

    Return results for months with no users

--- lib/admin_statistics_digest/new_user.rb
+++ lib/admin_statistics_digest/new_user.rb
@@ -18,9 +18,8 @@ period_new_users AS (
 SELECT
 p.months_ago,
 u.id AS user_id
-FROM
-users u
-JOIN periods p
+FROM users u
+RIGHT JOIN periods p
 ON u.created_at >= p.period_start
 AND u.created_at <= p.period_end
 ),
@@ -30,7 +29,7 @@ uv.user_id,
 p.months_ago,
 count(uv.user_id) as visits
 FROM user_visits uv
-JOIN periods p
+RIGHT JOIN periods p
 ON uv.visited_at >= p.period_start
 AND uv.visited_at <= p.period_end
 GROUP BY p.months_ago, uv.user_id
@@ -38,15 +37,15 @@ ORDER BY p.months_ago, visits DESC
 )
 
 SELECT
-puv.months_ago,
-count(1)
-FROM period_user_visits puv
-JOIN period_new_users pnu
-ON pnu.user_id = puv.user_id
-AND pnu.months_ago = puv.months_ago
-WHERE puv.visits >= #{filters.repeats}
-GROUP BY puv.months_ago
-ORDER BY puv.months_ago
+pnu.months_ago,
+count(puv.user_id) AS user_count
+FROM period_new_users pnu
+LEFT JOIN period_user_visits puv
+ON puv.months_ago = pnu.months_ago
+AND puv.user_id = pnu.user_id
+AND puv.visits >= #{filters.repeats}
+GROUP BY pnu.months_ago
+ORDER BY pnu.months_ago
     SQL
   end
 
