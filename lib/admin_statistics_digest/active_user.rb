b7bb0aa29ebf2c77700398ab83a3a3d2950384a5
scossar <scossar3@gmail.com>
2017-12-03 16:52:03 -0800

    Return results for months with no active users

--- lib/admin_statistics_digest/active_user.rb
+++ lib/admin_statistics_digest/active_user.rb
@@ -1,34 +1,33 @@
 require_relative '../admin_statistics_digest/base_report'
 
 class AdminStatisticsDigest::ActiveUser < AdminStatisticsDigest::BaseReport
-
   provide_filter :months_ago
 
   def to_sql
     <<~SQL
-WITH "periods" AS (
+WITH periods AS (
 SELECT
-"months_ago",
-date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * "months_ago" AS "period_start",
-date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * "months_ago" + INTERVAL '1 month' - INTERVAL '1 second' AS "period_end"
-FROM unnest(ARRAY #{filters.months_ago}) as "months_ago"
+months_ago,
+date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago AS period_start,
+date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago + INTERVAL '1 month' - INTERVAL '1 second' AS period_end
+FROM unnest(ARRAY #{filters.months_ago}) as months_ago
 ),
-"visiting_users" AS (
+visiting_users AS (
 SELECT
-DISTINCT "uv"."user_id" AS "user_id",
-"p"."months_ago" AS "months_ago"
-FROM "user_visits" "uv"
-JOIN "periods" "p"
-ON "uv"."visited_at" >= "p"."period_start"
-AND "uv"."visited_at" <= "p"."period_end"
+DISTINCT uv.user_id,
+p.months_ago
+FROM user_visits uv
+RIGHT JOIN periods p
+ON uv.visited_at >= p.period_start
+AND uv.visited_at <= p.period_end
 )
 
 SELECT
-count(1) AS "monthly_active_users",
-"vu"."months_ago" AS "months_ago"
-FROM "visiting_users" "vu"
-GROUP BY "vu"."months_ago"
-ORDER BY "vu"."months_ago"
+vu.months_ago,
+count(vu.user_id) as active_users
+FROM visiting_users vu
+GROUP BY vu.months_ago
+ORDER BY vu.months_ago
 SQL
   end
 end
