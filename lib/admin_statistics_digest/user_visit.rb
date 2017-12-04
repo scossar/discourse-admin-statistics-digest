9b5957de8d29a3fa9f17878e0d0cc0114082e2ec
scossar <scossar3@gmail.com>
2017-12-03 16:59:53 -0800

    Return results for months with no visits

--- lib/admin_statistics_digest/user_visit.rb
+++ lib/admin_statistics_digest/user_visit.rb
@@ -6,29 +6,23 @@ class AdminStatisticsDigest::UserVisit < AdminStatisticsDigest::BaseReport
 
   def to_sql
     <<~SQL
-WITH "periods" AS (
+WITH periods AS (
 SELECT
-"months_ago",
-date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * "months_ago" AS "period_start",
-date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * "months_ago" + INTERVAL '1 month' - INTERVAL '1 second' AS "period_end"
-FROM unnest(ARRAY #{filters.months_ago}) as "months_ago"
-),
-"visiting_users" AS (
-SELECT
-"uv"."user_id" AS "user_id",
-"p"."months_ago" AS "months_ago"
-FROM "user_visits" "uv"
-JOIN "periods" "p"
-ON "uv"."visited_at" >= "p"."period_start"
-AND "uv"."visited_at" <= "p"."period_end"
+months_ago,
+date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago AS period_start,
+date_trunc('month', CURRENT_DATE) - INTERVAL '1 months' * months_ago + INTERVAL '1 month' - INTERVAL '1 second' AS period_end
+FROM unnest(ARRAY #{filters.months_ago}) as months_ago
 )
 
 SELECT
-count(1) AS "monthly_user_visits",
-"vu"."months_ago" AS "months_ago"
-FROM "visiting_users" "vu"
-GROUP BY "vu"."months_ago"
-ORDER BY "vu"."months_ago"
+p.months_ago,
+count(uv.user_id) AS user_visits
+FROM user_visits uv
+RIGHT JOIN periods p
+ON uv.visited_at >= p.period_start
+AND uv.visited_at <= p.period_end
+GROUP BY p.months_ago
+ORDER BY p.months_ago
     SQL
   end
 end
